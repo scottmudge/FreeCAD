45421136aa0ca4588446676831ef99b6e2a3b507
Zheng, Lei <realthunder.dev@gmail.com>
2021-03-31 10:54:44 +0800

    PartDesign: exclude wrapper checking on allowed PartDesign object

--- src/Mod/PartDesign/Gui/Utils.cpp
+++ src/Mod/PartDesign/Gui/Utils.cpp
@@ -1003,10 +1003,8 @@ public:
             return;
         auto type = object.getTypeId();
         if (!type.isDerivedFrom(Part::Feature::getClassTypeId())
-                || type.isDerivedFrom(PartDesign::Feature::getClassTypeId())
                 || type.isDerivedFrom(PartDesign::Body::getClassTypeId())
-                || type.isDerivedFrom(PartDesign::ShapeBinder::getClassTypeId())
-                || type.isDerivedFrom(Part::SubShapeBinder::getClassTypeId())
+                || PartDesign::Body::isAllowed(type)
                 || object.hasExtension(App::LinkBaseExtension::getExtensionClassTypeId()))
             return;
 
