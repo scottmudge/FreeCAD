f3e0ae26db17457ab7336c0f313d8edbe2b96134
Zheng, Lei <realthunder.dev@gmail.com>
2021-04-16 16:45:55 +0800

    App/Gui: add APIs for for efficient extension iteration
    ExtensionContainer::foreach/query/callExtension()

--- src/Gui/Document.cpp
+++ src/Gui/Document.cpp
@@ -1485,6 +1485,8 @@ void Document::slotFinishRestoreObject(const App::DocumentObject &obj) {
         vpd->finishRestoring();
         if(!vpd->canAddToSceneGraph())
             toggleInSceneGraph(vpd);
+        else if (vpd->Visibility.getValue())
+            vpd->setModeSwitch();
     }
 }
 
