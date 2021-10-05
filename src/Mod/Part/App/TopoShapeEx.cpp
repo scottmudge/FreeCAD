68ec64a34b3fea07143b97f68850736109fae9bf
Zheng, Lei <realthunder.dev@gmail.com>
2021-10-05 17:21:56 +0800

    Part: check null shape in TopoShape::makEFace()

--- src/Mod/Part/App/TopoShapeEx.cpp
+++ src/Mod/Part/App/TopoShapeEx.cpp
@@ -2270,6 +2270,8 @@ TopoShape &TopoShape::makEFace(const TopoShape &shape,
                                const gp_Pln *pln)
 {
     std::vector<TopoShape> shapes;
+    if (shape.isNull())
+        HANDLE_NULL_SHAPE;
     if(shape.getShape().ShapeType() == TopAbs_COMPOUND)
         shapes = shape.getSubTopoShapes();
     else
